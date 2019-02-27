922943dcbf385e8a24d79250bbc03598de367610
janezhu1618 <janezhu@pursuit.org>
2019-02-27 10:33:44 -0500

    set up message board vc and message custom table view cell

---
+++ JobOrganizer/Views/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  JobOrganizer
+//
+//  Created by Jane Zhu on 2/27/19.
+//  Copyright © 2019 JaneZhu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
